8b159ac72c35a20e3865204f5096b97b3ee6447d
Gutz <darckProyect8@gmail.com>
2024-07-17 22:13:49 -0500

    nothing

---
+++ microservice-customer/src/main/java/com/zzootec/customer/util/KeycloakProvider.java
@@ -0,0 +1,16 @@
+package com.zzootec.customer.util;
+
+public class KeycloakProvider {
+
+    private static final String SERVER_URL = "http://localhost:9099";
+    private static final String REALM_NAME = "zzootec";
+    private static final String REALM_MASTER = "master";
+    private static final String ADMIN_CLI = "admin-cli";
+    private static final String USER_CONSOLE = "admin";
+    private static final String PASSWORD_CONSOLE = "admin";
+    private static final String CLIENT_SECRET = "j2lq3G3BjPpZDHyzf1VGpvYxQDfRvXuY";
+
+
+
+
+}
